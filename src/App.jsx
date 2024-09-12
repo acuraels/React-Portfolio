c853a6ad979352519dd84ff4a2b169b0de316680
acuraels <daniil.rib@mail.ru>
2024-09-13 00:12:56 +0500

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Header from './components/Header.jsx';
+import './styles/app.css'
+
+function App() {
+  return (
+    <Header />
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,51 @@
+// src/components/Header.jsx
+
+import React from 'react';
+import "./../styles/header.css";
+
+const Header = () => {
+    const toggleMenu = () => {
+        const menu = document.querySelector('.dropdown-menu');
+        menu.classList.toggle('active');
+    };
+
+    return (
+        <header className="header">
+            <div className="header-container">
+                <nav className="header-nav">
+                    <div className="title">
+                        <div className="burger-menu" onClick={toggleMenu}>
+                            <div className="burger-line"></div>
+                            <div className="burger-line"></div>
+                            <div className="burger-line"></div>
+                        </div>
+                        <div className="dropdown-menu">
+                            <a href="index.html">English</a>
+                            <a href="html/ru-index.html">Русский</a>
+                        </div>
+                    </div>
+                    <ul className="nav-links">
+                        <li className="link">
+                            <a href="#about-me">About me</a>
+                        </li>
+                        <li className="link">
+                            <a href="#my-skills">Skills</a>
+                        </li>
+                        <li className="link">
+                            <a href="#my-projects">Projects</a>
+                        </li>
+                        <li className="link">
+                            <a href="#my-experience">Experience</a>
+                        </li>
+                        <li className="link">
+                            <a href="#contact">Contact</a>
+                        </li>
+                    </ul>
+                    <a className="btn-nav" href="html/hire.html">Hire me</a>
+                </nav>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
